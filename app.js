130dec062295d254022d3ca9c4efe83c8404351e
dlucianop <3112dlp@gmail.com>
2025-03-18 10:05:00 -0600

    Se agrego la funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades 
+// en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const listaAmigos = document.getElementById('listaAmigos');
+const resultado = document.getElementById('resultado');
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    if (!amigo || amigo.trim() === ''){
+        alert("ERROR. Ingrese un nombre valido");
+    } else{
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    }
+}
+
+function sortearAmigo(){
+
+}
