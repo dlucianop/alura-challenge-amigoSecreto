bcd57be058cf9bbc5342790d531e604862058827
dlucianop <3112dlp@gmail.com>
2025-03-18 10:27:05 -0600

    Se agrego la funcion sortearAmigo y se corrigieron minimos detalles en la funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades 
 // en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 const listaAmigos = document.getElementById('listaAmigos');
-const resultado = document.getElementById('resultado');
+const arrayAmigos = [];
 
 function agregarAmigo(){
-    let amigo = document.getElementById('amigo').value;
+    let inputAmigo = document.getElementById('amigo');
+    let amigo = inputAmigo.value;
     if (!amigo || amigo.trim() === ''){
         alert("ERROR. Ingrese un nombre valido");
+        inputAmigo.value = "";
     } else{
-        const li = document.createElement("li");
+        let li = document.createElement("li");
         li.textContent = amigo;
         listaAmigos.appendChild(li);
+        arrayAmigos.push(amigo);
+        inputAmigo.value = "";
     }
 }
 
 function sortearAmigo(){
-
+    const resultado = document.getElementById('resultado');
+    if (arrayAmigos.length === 0) {
+        alert('ERROR. No tiene amigos en la lista');
+    } else{
+        const indiceAleatorio = Math.floor(Math.random() * arrayAmigos.length);
+        resultado.textContent = arrayAmigos[indiceAleatorio];
+    }
 }
